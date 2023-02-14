155f5c3baed4f02aea3878ab50cce7afffcc6417
wahabfaisal <mdfaisalwahab@gmail.com>
2023-02-14 19:21:15 +0300

    Day - 13

---
+++ Day 13/practice.js
@@ -0,0 +1,14 @@
+// JS Built-in Properties and Methods
+
+// Length of a String
+
+const greetings = "Hello, Good Evening!";
+console.log(greetings.length); // 20
+
+// Convert All character to UPPERCASE
+
+console.log(greetings.toUpperCase()); // HELLO, GOOD EVENING!
+
+// Convert All character to lowercase
+
+console.log(greetings.toLowerCase()); // hello, good evening!
